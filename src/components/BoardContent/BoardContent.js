dd5a0700e25d8c10fc4e59e97e646bfaae3373f2
Chien <chienndsl@gmail.com>
2021-12-25 15:17:10 +0700

    Clean React Architecture.

---
+++ src/components/BoardContent/BoardContent.js
@@ -0,0 +1,14 @@
+import  React from 'react'
+
+import './BoardContent.scss'
+import Column from 'components/Column/Column'
+
+function BoardContent(){
+    return (
+        <nav className="board-content">
+          <Column/>
+      </nav>
+    )
+}
+
+export default BoardContent
