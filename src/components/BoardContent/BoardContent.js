4110a3662570cac14399fccc0981fd42f04da8bd
Chien <chienndsl@gmail.com>
2021-12-29 01:37:52 +0700

    Initial sample data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,14 +1,39 @@
-import  React from 'react'
+import  React, { useState, useEffect } from 'react'
+import {isEmpty} from 'lodash'
 
 import './BoardContent.scss'
 import Column from 'components/Column/Column'
+import {initialData} from 'actions/initialData'
+import {mapOrder} from 'utilities/sort'
 
 function BoardContent(){
+    const [board, setBoard]= useState({}) 
+    const [columns, setColumns]= useState([])
+
+    useEffect(() => {
+      const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
+      if(boardFromDB){
+        setBoard(boardFromDB)
+        
+        // //sort
+        // boardFromDB.columns.sort((a,b) => {
+        //   return boardFromDB.columnOrder.indexOf(a.id) - boardFromDB.columnOrder.indexOf(b.id)
+        // })
+        setColumns(mapOrder(boardFromDB.columns,boardFromDB.columnOrder,'id'))
+
+      }
+    },[])
+
+    if(isEmpty(board)){
+      return <div className="not-found" style={{'padding':'10px', 'color':'white' }}>Board not found!</div>
+    }
+
     return (
         <nav className="board-content">
-          <Column/>
+          {columns.map((column, index) => <Column key={index}  column={column} />)}
       </nav>
     )
+   
 }
 
 export default BoardContent
